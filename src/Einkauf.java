ed2bfd9ea7adebc21e64855a393740d647c06d9d
presteld <dietmar.prestel@hs-kempten.de>
2024-12-06 14:58:46 +0100

    Änderungen in GUI-Struktur

---
+++ src/Einkauf.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Einkauf extends JFrame {
+    private JPanel jpPanel;
+
+
+
+
+    public Einkauf() throws HeadlessException {
+
+setTitle("Einkauf");
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setSize(600,400);
+setContentPane(jpPanel);
+setVisible(true);
+
+
+
+
+
+    }
+
+
+}
